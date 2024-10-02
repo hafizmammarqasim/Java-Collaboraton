5f7abbd386b54adbf698d44f564edf2d0c3b609c
Ammar <ammarhafiz222@gmail.com>
2024-10-02 23:36:10 +0500

    Product File from Ammar

---
+++ Product.java
@@ -0,0 +1,12 @@
+public class Product{
+	private String name;
+	private int price;
+	private int quantity;
+
+	Product(String name, int price, int quantity){
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity; 
+	} 
+
+}
