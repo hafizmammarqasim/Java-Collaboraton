8fce9f06a3ea522e278d31293ff42dcea52a09c0
D1Ahmed <dawoodahmed653356@gmail.com>
2024-10-03 00:37:22 +0500

    Resolved merge conflict in Product.java

--- Product.java
+++ Product.java
@@ -1,3 +1,17 @@
+<<<<<<< HEAD
+public class Product{
+	private String name;
+	private int price;
+	private int quantity;
+	private String id;
+
+	Product(String name, int price, int quantity){
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity; 
+	} 
+
+=======
 public class Product
 {
 String name="Sabun";
@@ -15,4 +29,5 @@ int quantity;
 	}
 	
 		
+>>>>>>> ccfeb365b4e05d79d071273779af010cd52448ab
 }
