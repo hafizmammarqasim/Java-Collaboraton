81c65ea490d22e659ba022c3a8377f017717ea1f
D1Ahmed <dawoodahmed653356@gmail.com>
2024-10-03 02:16:24 +0500

    Added getter and setters for area

--- City.java
+++ City.java
@@ -1,4 +1,16 @@
 public class City{
 	private String name;
+	private int area;
+	
+	
+	public void setArea(int area)
+	{
+		this.area=area;	
+	}
+	
+	public void getArea()
+	{
+		return area;
+	}
 
 }
